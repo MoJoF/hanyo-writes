554e869f478fc22349d3d974c107ef19e7a15333
MoJoF <mojo.13081998@gmail.com>
2025-12-12 21:04:05 +0200

    Создание страницы с созданием постов и списка постов

---
+++ admin/auth.js
@@ -0,0 +1,23 @@
+(function () {
+    const authData = localStorage.getItem("auth")
+    const url = atob("aHR0cHM6Ly9oYW55by13cml0ZXMub215cmF1Y3kud29ya2Vycy5kZXYvYXV0aA==")
+    const headers = { "Content-Type": "application/json" }
+    // Если сохранилась запись об авторизации
+    if (authData) {
+        fetch(url, {
+            method: "POST",
+            headers,
+            body: JSON.stringify({ authData })
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error)
+                    document.location.href = "/admin"
+                }
+            })
+    } else {
+        alert("You are not logged")
+        document.location.href = "/admin"
+    }
+})()
