159200ed3065482b6710953406b2de4499777cdd
MoJoF <mojo.13081998@gmail.com>
2025-12-14 18:54:15 +0200

    Correct index.html

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ function cat_render(categories) {
     const sectionCats = document.querySelector(".sections")
 
     categories
-        .filter(cat => cat.parent_key === "none")
+        .filter(cat => cat.parent_id === "none")
         .forEach(c => {
             const contBlock = document.createElement("div")
             contBlock.classList = "block"
 
-            const childCats = categories.filter(cat_child => cat_child.parent_key === c.key)
+            const childCats = categories.filter(cat_child => cat_child.parent_id === c.parent_id)
 
             contBlock.innerHTML = `
-                <a href="/category?link=${c.link}"><h3>${c.title}</h3></a>
+                <a href="/category?link=${c.category_link}"><h3>${c.category_title}</h3></a>
             `
             const listLinks = document.createElement("div")
             listLinks.classList = "list-links"
 
             childCats.forEach(el => {
                 const subCat = document.createElement("a")
-                subCat.href = `/category?link=${el.link}`
-                subCat.textContent = el.title
+                subCat.href = `/category?link=${el.category_link}`
+                subCat.textContent = el.category_title
                 listLinks.appendChild(subCat)
             })
 
