4a3c1219f0a4a4668fddb4ce7bf524e8872f1cf7
MoJoF <mojo.13081998@gmail.com>
2025-12-15 20:54:46 +0200

    Рендер постов у обычных (не родительских) категорий

--- category/category.js
+++ category/category.js
@@ -3,6 +3,26 @@ const urlAddress = atob("aHR0cHM6Ly9oYW55by13cml0ZXMub215cmF1Y3kud29ya2Vycy5kZXY
 const url = new URL(window.location.href)
 const link = url.searchParams.get("link")
 
+const postsRender = posts => {
+    const container = document.querySelector('main')
+
+    if (posts.length === 0) {
+        container.textContent = "Здесь пока ничего нет"
+    }
+
+    posts.forEach(postObj => {
+        const postCont = document.createElement('div')
+        postCont.className = "post"
+
+        const postHeader = document.createElement('a')
+        postHeader.href = `/post?id=${postObj.post_id}`
+        postHeader.textContent = postObj.post_title
+
+        postCont.appendChild(postHeader)
+        container.appendChild(postCont)
+    });
+}
+
 const parentCategoryRender = data => {
 
 }
@@ -24,6 +44,20 @@ const childCategoryRender = data => {
     categoryBlock.appendChild(categoryHeader)
 
     container.appendChild(categoryBlock)
+
+    fetch(urlAddress, {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify({ "action": "get_posts_by_category", category: data.child })
+            .then(resp => resp.json())
+            .then(d => {
+                if (d.status === "OK") {
+                    postsRender(d.posts)
+                }
+            })
+    })
 }
 
 
